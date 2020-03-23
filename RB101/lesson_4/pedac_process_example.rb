ec451601e9cbcfc09474b65f06cd38847c9bce48
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-03-23 18:32:23 +0100

    Fix typo

--- RB101/lesson_4/pedac_process_example.rb
+++ RB101/lesson_4/pedac_process_example.rb
@@ -98,7 +98,7 @@ end
 def substrings(str)
   result = []
   0.upto(str.size - 2).each do |start_idx|
-    (start_idx + 1).upto (str.size - 1) do |end_idx|
+    (start_idx + 1).upto(str.size - 1) do |end_idx|
       result << str[start_idx..end_idx]
     end
   end
