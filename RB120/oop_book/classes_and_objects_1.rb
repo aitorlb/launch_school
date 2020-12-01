b3eeb91e1a6e7bb329bff8ad59083eea1bc9acd1
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-12-01 22:23:12 +0100

    [RB120] OOP book: Classes and Objects - Part I

---
+++ RB120/oop_book/classes_and_objects_1.rb
@@ -0,0 +1,57 @@
+=begin
+Exercises
+
+1. Create a class called MyCar. When you initialize a new instance or object 
+  of the class, allow the user to define some instance variables that tell us the year, color, and model of the car. Create an instance variable that is set to 0 during instantiation of the object to track the current speed of the car as well. Create instance methods that allow the car to speed up, brake, and shut the car off.
+
+2. Add an accessor method to your MyCar class to change and view the color of 
+  your car. Then add an accessor method that allows you to view, but not modify, the year of your car.
+
+3. You want to create a nice interface that allows you to accurately describe 
+  the action you want your program to perform. Create a method called 
+  spray_paint that can be called on an object and will modify the color of the 
+  car.
+=end
+
+class MyCar
+  attr_accessor :color
+  attr_reader   :year
+
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @current_speed = 0
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def spray_paint(color)
+    self.color = color
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+
+lumina = MyCar.new(1997, 'chevy lumina', 'white')
+lumina.speed_up(20)         #=> You push the gas and accelerate 20 mph.
+lumina.speed_up(20)         #=> You push the gas and accelerate 40 mph.
+lumina.brake(20)            #=> You push the brake and decelerate 20 mph.
+lumina.brake(20)            #=> You push the brake and decelerate 20 mph.
+lumina.shut_down            #=> Let's park this bad boy!
+lumina.color = 'black'
+puts lumina.color           #=> black
+puts lumina.year            #=> 1997
+lumina.spray_paint('red')   #=> Your new red paint job looks great!
