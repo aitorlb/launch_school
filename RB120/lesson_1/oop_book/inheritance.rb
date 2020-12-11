4b4c4d7efe77100ef84f7d3f12e446ab220b1b7b
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-12-11 08:25:10 +0100

    [RB120] Reorganize files

--- RB120/lesson_1/oop_book/inheritance.rb
+++ RB120/lesson_1/oop_book/inheritance.rb
@@ -32,26 +32,26 @@ Exercises
   grade getter public, so joe.grade will raise an error. Create a 
   better_grade_than? method, that you can call like so...
 
-```
-puts "Well done!" if joe.better_grade_than?(bob)
-```
+  ```
+  puts "Well done!" if joe.better_grade_than?(bob)
+  ```
 
 8. Given the following code...
 
-```
-bob = Person.new
-bob.hi
-```
+  ```
+  bob = Person.new
+  bob.hi
+  ```
 
-And the corresponding error message...
+  And the corresponding error message...
 
-```
-NoMethodError: private method `hi' called for #<Person:0x007ff61dbb79f0>
-from (irb):8
-from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
-```
+  ```
+  NoMethodError: private method `hi' called for #<Person:0x007ff61dbb79f0>
+  from (irb):8
+  from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+  ```
 
-What is the problem and how would you go about fixing it?
+  What is the problem and how would you go about fixing it?
 
 The problem is that the method hi is a private method, therefore it is 
 unavailable to the object. I would fix this problem by moving the hi method 
